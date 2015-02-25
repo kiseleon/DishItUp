0b6d11fbdac433ec06fee0d5a5b29902fb87c22a
Kevin Johnson <joh06018@byui.edu>
2015-02-25 08:30:47 -0700

    Added MVC classes Display, MissionControl, RecipeCard, and Rolodex

---
+++ app/src/main/java/com/example/cs246/dishitup/MissionControl.java
@@ -0,0 +1,7 @@
+package com.example.cs246.dishitup;
+
+/**
+ * Created by Kevin on 2/25/2015.
+ */
+public class MissionControl {
+}
---
+++ app/src/main/java/com/example/cs246/dishitup/RecipeCard.java
@@ -0,0 +1,12 @@
+package com.example.cs246.dishitup;
+
+/**
+ * Created by Kevin on 2/25/2015.
+ */
+public class RecipeCard {
+    private String name;
+    private int id;
+
+
+
+}
