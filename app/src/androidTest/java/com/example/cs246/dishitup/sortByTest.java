a7b4f184b3c8f4c3ef05ce4bca8f63d8823eb79a
TrentonH <trentonwhamilton@gmail.com>
2015-02-26 21:25:39 -0700

    did one of the asserts for the sortby function

--- app/src/androidTest/java/com/example/cs246/dishitup/sortByTest.java
+++ app/src/androidTest/java/com/example/cs246/dishitup/sortByTest.java
@@ -7,5 +7,25 @@ package com.example.cs246.dishitup;
 public class sortByTest {
     SortBy sortBy = new SortBy();
 
+    void runTest() {
+
+        if( TestTheName()){
+            System.out.println("it worked!!!");
+        }
+        else{
+            System.out.println("didn't work");
+        }
+
+    }
+
+
+
+    boolean TestTheName(){
+        boolean name = false;
+        String getname;
+        getname = sortBy.GetName("this is trenton");
+        assert getname.equals("this is trenton");
+      return   name;
+    }
 
 }
