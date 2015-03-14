fd13bd1560fce95d4adce7e882f0f18c25929731
Kevin Johnson <kevin.j.johnson257@gmail.com>
2015-03-14 15:53:15 -0600

    Removed tests depending on removed RecipeCard constructor.

--- app/src/androidTest/java/com/example/cs246/dishitup/RecipeCardTest.java
+++ app/src/androidTest/java/com/example/cs246/dishitup/RecipeCardTest.java
@@ -9,11 +9,6 @@ public class RecipeCardTest extends TestCase {
         assertEquals("No comment.", testing.getComment());
         testing.setComment("Setting comment...");
         assertEquals("Setting comment...", testing.getComment());
-
-        testing = new RecipeCard(60);
-        assertEquals("No comment at this time.", testing.getComment());
-        testing.setComment("Setting comment...");
-        assertEquals("Setting comment...", testing.getComment());
     }
 
     public void testGetCookTime() throws Exception {
@@ -21,11 +16,6 @@ public class RecipeCardTest extends TestCase {
         assertEquals(90, testing.getCookTime());
         testing.setCookTime(100);
         assertEquals(100, testing.getCookTime());
-
-        testing = new RecipeCard(60);
-        assertEquals(60, testing.getCookTime());
-        testing.setCookTime(1000);
-        assertEquals(1000, testing.getCookTime());
     }
 
     public void testAddCategory() throws Exception {
