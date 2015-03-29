e23913196efe9647c416c40120b62121fdea0aad
Kevin Johnson <joh06018@byui.edu>
2015-03-28 19:20:32 -0600

    Added a Singleton for the filter data. (Not yet implemented)

--- app/src/main/java/com/example/cs246/dishitup/FilterData.java
+++ app/src/main/java/com/example/cs246/dishitup/FilterData.java
@@ -4,6 +4,12 @@ package com.example.cs246.dishitup;
  * Created by Kevin on 3/28/2015.
  */
 public class FilterData {
+    private static FilterData ourInstance = new FilterData();
 
+    public static FilterData getInstance() {
+        return ourInstance;
+    }
 
+    private FilterData() {
+    }
 }
