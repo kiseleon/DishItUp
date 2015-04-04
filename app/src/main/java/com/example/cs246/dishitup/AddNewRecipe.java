9dc7955ebe521b561af262a2c83d305f57378dd6
TrentonH <trentonwhamilton@gmail.com>
2015-04-03 21:11:46 -0600

    added take pickture. works but doesnt save to database

--- app/src/main/java/com/example/cs246/dishitup/AddNewRecipe.java
+++ app/src/main/java/com/example/cs246/dishitup/AddNewRecipe.java
@@ -2,21 +2,43 @@ package com.example.cs246.dishitup;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.app.Activity;
+import android.os.Environment;
+import android.provider.MediaStore;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TooManyListenersException;
+
+// take a photo
+// code was based on this website
+//http://www.c-sharpcorner.com/UploadFile/e14021/capture-image-from-camera-and-selecting-image-from-gallery-o/
+
 
 /**
  * A class for getting input from the user to create a recipe
@@ -33,6 +55,9 @@ public class AddNewRecipe extends ActionBarActivity {
     EditText comments;
     EditText categories;
     EditText amount;
+//trenton added this
+    ImageView recipePicture;
+    Button addImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +73,136 @@ public class AddNewRecipe extends ActionBarActivity {
         comments = (EditText)findViewById(R.id.editComments);
         categories = (EditText)findViewById(R.id.categoriesField);
         recipeCard = new RecipeCard();
+
+
+
+
+
+
+        addImageButton=(Button)findViewById(R.id.addImageButton);
+        recipePicture=(ImageView)findViewById(R.id.recipePicture);
+        addImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
     }
+
+
+
+
+
+
+
+
+    private void selectImage() {
+
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(AddNewRecipe.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo"))
+                {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    startActivityForResult(intent, 1);
+                }
+                else if (options[item].equals("Choose from Gallery"))
+                {
+                    Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+
+                }
+                else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+    //if they hit take photo
+            if (requestCode == 1) {
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    Bitmap bitmap;
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            bitmapOptions);
+
+                    recipePicture.setImageBitmap(bitmap);
+
+                    String path = android.os.Environment
+                            .getExternalStorageDirectory()
+                            + File.separator
+                            + "Phoenix" + File.separator + "default";
+                    f.delete();
+                    OutputStream outFile = null;
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+
+                    //need to get the string path here and pass it in... no idea how yet
+                    // this is static setPickref is not == problem
+                    //RecipeCard.setPictureRef(String.valueOf(System.currentTimeMillis()) + ".jpg");
+                   // RecipeCard.setPictureRef(file.getName());
+
+
+                    try {
+                        outFile = new FileOutputStream(file);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                        outFile.flush();
+                        outFile.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+    // if they hit galorie
+            } else if (requestCode == 2) {
+
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+
+                //this is what i added the problem is picturePath is static...
+                //RecipeCard.setPictureRef(picturePath);
+
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                Log.w("path of image from gallery......******************.........", picturePath+"");
+                recipePicture.setImageBitmap(thumbnail);
+            }
+        }
+    }
+
+
+
+
+
+
 
     /**
      * Adds the category from the category field to the RecipeCard
@@ -127,7 +281,9 @@ public class AddNewRecipe extends ActionBarActivity {
         ingredientView.setText(output);
     }
 
+private void addImage(){
 
+}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
